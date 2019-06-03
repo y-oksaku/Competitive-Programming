e8d93bb34b2f38db84851b4cfc608f88d040ccf9
y-oksaku <y.oksaku@stu.kanazawa-u.ac.jp>
2019-06-03 22:00:02 +0900

    cppテンプレートファイル 作成

---
+++ code/temp/temp.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+#include <algorithm>
+#include <math.h>
+#include <iomanip>
+using namespace std;
+
+int main(void) {
+    string s;
+    cin >> s;
+
+    cout << s;
+
+    return 0;
+}
