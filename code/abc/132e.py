86ab5a446e80e726da05d8b5854f536329615928
y-oksaku <y.oksaku@stu.kanazawa-u.ac.jp>
2019-07-03 12:09:58 +0900

    abc131 e 追加

--- code/abc/132e.py
+++ code/abc/132e.py
@@ -1,19 +1,54 @@
-from collections import deque
+def dijkstra(edge,start,end) :
+    import heapq
+
+    minCosts = [float('inf') for _ in range(len(edge))]
+
+    nodeHeap = []
+    heapq.heappush(nodeHeap, (0, start))  # (cost, node)
+
+    while nodeHeap :
+        cost, node = heapq.heappop(nodeHeap)
+
+        if minCosts[node] <= cost :
+            continue
+
+        minCosts[node] = cost
+
+        for nextNode , weight in edge[node] :
+            newCost = cost + weight
+            heapq.heappush(nodeHeap, (newCost, nextNode))
+    dist = minCosts[endNode]
+    if dist == float('inf') :
+        return -1
+    return dist
 
 N , M = map(int,input().split())
 
-to = [[]] * M
+# edges , costs
+nodes = [[] for _ in range(3 * N)]
 
-for i in range(N) :
-    u , v = list(map(int,input().split()))
-    to[u].append(v)
+# 辺の入力
+for _ in range(M) :
+    fromNode , toNode = map(int,input().split())
+    fromNode -= 1
+    toNode -= 1
+    fromNode *= 3
+    toNode *= 3
 
-S , T = map(int,input())
+    nodes[fromNode].append((toNode + 1,1))
+    nodes[fromNode + 1].append((toNode + 2,1))
+    nodes[fromNode + 2].append((toNode,1))
 
-q = deque([S])
+# スタート ゴール
+startNode , endNode = map(int,input().split())
+startNode -= 1
+endNode -= 1
+startNode *= 3
+endNode *= 3
 
-while q :
-    now = q.popleft()
+ans = dijkstra(nodes,startNode,endNode)
 
-
-
+if ans == -1 :
+    print(ans)
+else :
+    print(ans // 3)
