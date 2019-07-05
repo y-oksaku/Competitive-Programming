2e0fefb0fb6cb23e7062f3183d0759d1fc2435da
y-oksaku <y.oksaku@stu.kanazawa-u.ac.jp>
2019-07-05 23:22:14 +0900

    abc125 c 追加

--- code/abc/125c.py
+++ code/abc/125c.py
@@ -1,21 +1,37 @@
-import math
+def gcd(a,b) :
+    r = a % b
+    if r == 0 :
+        return b
+    else :
+        return gcd(b,r)
 
 N = int(input())
+
 A = list(map(int,input().split()))
+
+L = [0] * N
+R = [0] * N
+
+for i in range(N) :  # iより左(右)
+    if i == 0 :
+        L[i] = A[i]
+        R[-i-1] = A[-i-1]
+        R[-i-1] = A[-i-1]
+        R[-i-1] = A[-i-1]
+    else :
+        L[i] = gcd(L[i-1], A[i-1])
+        R[-i-1] = gcd(R[-i],A[-i])
 
 ans = 1
 
-if N == 1 :
-    ans = A[0]
-else :
-    for i in range(N) :
-        B = A[:i-1] + A[i:] # i番目を除く
-        stat = B[0]
-        for b in B[1:] :
-            stat = math.gcd(stat,b)
-            if stat == 1:
-                break
-        else :
-            ans = max(ans,stat)
+for i in range(N) :
+    if i == 0 :
+        new = R[i]
+    elif i == N-1 :
+        new = L[i]
+    else :
+        new = gcd(L[i],R[i])
+    if ans < new :
+        ans = new
 
 print(ans)
