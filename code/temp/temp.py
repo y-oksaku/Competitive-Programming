2f85f888b0a0817f0fc02ae53cc49f5b52e88e21
y-oksaku <y.oksaku@stu.kanazawa-u.ac.jp>
2019-07-09 16:53:34 +0900

    テンプレート更新 python

--- code/temp/temp.py
+++ code/temp/temp.py
@@ -15,3 +15,37 @@ def combination(n,r) :
 
 def ceil(a,b) :
     return (a + b - 1) // b
+
+class Combination :
+    def combi(self, n, k) :
+        if n < k :
+            return 1
+        factK = 1
+        factP = 1
+        for i in range(1, k+1) :
+            factK *= i
+            factP *= n-i+1
+
+        return factP // factK
+
+    def permutation(self, n, k) :
+        if n < k :
+            return 1
+        fact = 1
+        for i in range(k) :
+            fact *= n-i
+        return fact
+
+# MODにおける逆元
+def modInv(a, MOD=1000000007) :
+    b = MOD
+    u = 1
+    v = 0
+    while b :
+        t = a // b
+        a -= t * b
+        u -= t * v
+        a, b = b, a
+        u, v = v, u
+    u = u % MOD
+    return u
