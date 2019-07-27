8850449f48d16b1e94579864b5dc2950bb0c3144
y-oksaku <y.oksaku@stu.kanazawa-u.ac.jp>
2019-07-27 23:13:20 +0900

    テンプレート更新 C++ ctoi関数 追加

--- code/template/c++/main.cpp
+++ code/template/c++/main.cpp
@@ -27,6 +27,22 @@ const llong INF = 1e17;
 #define FORS(i, a, b) for (llong i = a; i < b; i++)
 #define FORR(i, n) for (llong i = n; i > 0; i++)
 
+llong ctoi(char c) {
+    switch (c) {
+        case '0': return 0;
+        case '1': return 1;
+        case '2': return 2;
+        case '3': return 3;
+        case '4': return 4;
+        case '5': return 5;
+        case '6': return 6;
+        case '7': return 7;
+        case '8': return 8;
+        case '9': return 9;
+        default: return 0;
+    }
+};
+
 int main(void) {
     cin.tie(0);
     ios::sync_with_stdio(false);
