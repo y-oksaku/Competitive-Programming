defc785003b5bd75e0a54f1b0ff17a05f6d259cc
y-oksaku <y.oksaku@stu.kanazawa-u.ac.jp>
2019-07-27 23:27:24 +0900

    abc 135d

---
+++ code/abc/135d_2.py
@@ -0,0 +1,24 @@
+S = input()
+MOD = 10**9 + 7
+lenS = len(S)
+
+dp = [[0 for _ in range(13)] for _ in range(lenS + 1)]  # dp[digit][d] = digit桁までの mod=d の数
+dp[0][0] = 1
+
+digitMask = 1
+
+for digit in range(1, lenS + 1) :
+    if(S[-digit] == '?') :
+        for d in range(13) :
+            for k in range(10) :
+                dp[digit][d] = (dp[digit][d] + dp[digit - 1][(d - k * digitMask) % 13]) % MOD
+    else :
+        a = int(S[-digit])
+        for d in range(13) :
+            dp[digit][d] = (dp[digit][d] + dp[digit - 1][(d - a * digitMask) % 13]) % MOD
+    digitMask *= 10
+    digitMask %= 13
+
+ans = (dp[len(S)][5]) % MOD
+print(ans)
+
