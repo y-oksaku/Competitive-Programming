ef62b42f1dc0d55765074bfd2d8d2c5f6e5bf314
y-oksaku <y.oksaku@stu.kanazawa-u.ac.jp>
2019-07-29 21:26:06 +0900

    テンプレート更新 C++ chmax, chmin追加

--- code/template/c++/main.cpp
+++ code/template/c++/main.cpp
@@ -11,6 +11,8 @@
 #include <iomanip>
 
 using namespace std;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
 
 typedef unsigned int uint;
 typedef long long llong;
