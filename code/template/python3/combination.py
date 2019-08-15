9b87d41041f0840341f01e0a796a197863e7eb72
y-oksaku <y.oksaku@stu.kanazawa-u.ac.jp>
2019-08-15 13:04:23 +0900

    テンプレート更新 python combination

--- code/template/python3/combination.py
+++ code/template/python3/combination.py
@@ -1,40 +1,22 @@
-# 階乗
-def factorial(n = 0) :
-    ret = [1] * n
-    for i in range(1,n) :
-        ret[i] = ret[i-1] * i
-    return ret
+class Combination:
+    def __init__(self, size, mod=10**9 + 7):
+        self.size = size + 2
+        self.mod = mod
+        self.fact = [1, 1] + [0] * size
+        self.inv = [1, 1] + [0] * size
+        self.factInv = [0, 1] + [0] * size
 
-N = 10
-fact = factorial(N)
+        for i in range(2, self.size):
+            self.fact[i] = self.fact[i - 1] * i % self.mod
+            self.inv[i] = -self.inv[self.mod % i] * (self.mod // i) % self.mod
+            self.factInv[i] = self.factInv[i - 1] * self.inv[i] % self.mod
 
-# 前計算を用いた組み合わせ計算
-def permutation(n,r) :
-    return fact[n] // fact[n-r]
+    def npr(self, n, r):
+        if n < r or n < 0 or r < 0:
+            return 0
+        return self.fact[n] * self.factInv[n - r] % self.mod
 
-def combination(n,r) :
-    return fact[n] // fact[n-r] // fact[r]
-
-def ceil(a,b) :
-    return (a + b - 1) // b
-
-# 組み合わせ計算
-class Combination :
-    def combi(self, n, k) :
-        if n < k :
-            return 1
-        factK = 1
-        factP = 1
-        for i in range(1, k+1) :
-            factK *= i
-            factP *= n-i+1
-
-        return factP // factK
-
-    def permutation(self, n, k) :
-        if n < k :
-            return 1
-        fact = 1
-        for i in range(k) :
-            fact *= n-i
-        return fact
+    def ncr(self, n, r):
+        if n < r or n < 0 or r < 0:
+            return 0
+        return self.fact[n] * (self.factInv[r] * self.factInv[n - r] % self.mod) % self.mod
