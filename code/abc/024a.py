dcdb862fedef191836d74ab76983cf2ab2c91d20
y-oksaku <y.oksaku@stu.kanazawa-u.ac.jp>
2019-08-19 07:29:41 +0900

    早解きラジオ体操 11

---
+++ code/abc/024a.py
@@ -0,0 +1,12 @@
+def sol():
+    A, B, C, K = map(int, input().split())
+    S, T = map(int, input().split())
+
+    ans = A * S + B * T
+    if S + T >= K:
+        ans -= (S + T) * C
+
+    print(ans)
+
+
+sol()
---
+++ code/other/code_thanks_festival_2019A_a.py
@@ -0,0 +1,6 @@
+def sol():
+    A, B = map(int, input().split())
+    print(4 * A + 2 * B)
+
+
+sol()
