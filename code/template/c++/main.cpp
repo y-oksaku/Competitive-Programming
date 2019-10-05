d7c593f71f01cf42d89733b9f18742e1078217cf
y-oksaku <y.oksaku@stu.kanazawa-u.ac.jp>
2019-10-05 15:54:17 +0900

    テンプレート更新 c++ main.cpp

--- code/template/c++/main.cpp
+++ code/template/c++/main.cpp
@@ -14,13 +14,26 @@ using namespace std;
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
 
-typedef unsigned int uint;
-typedef long long llong;
-typedef unsigned long long ullong;
-typedef long double ldouble;
+using Int = unsigned int;
+using llong = long long;
+using Llong = unsigned long long;
+using ldouble = long double;
+using intV = vector<int>;
+using llongV = vector<long long>;
+using llongVV = vector<vector<long long>>;
+using Graph = vector<vector<long long>>;
+using costGraph = vector<vector<pair<long long, long long>>>;
 
-typedef vector<llong> vecllong;
-typedef vector<vecllong> vvecllong;
+struct Edge {
+    long long from;
+    long long to;
+    long long cost;
+};
+
+template<typename T>
+using asc = less<T>();
+template<typename T>
+using desc = greater<T>();
 
 const llong MOD = 1e9 + 7;
 const llong INF = 1e17;
@@ -28,24 +41,6 @@ const llong INF = 1e17;
 #define FOR(i, n) for (llong i = 0; i < n; i++)
 #define FORS(i, a, b) for (llong i = a; i < b; i++)
 #define FORR(i, n) for (llong i = n; i > 0; i++)
-#define asc less<llong>()
-#define desc greater<llong>()
-
-llong ctoi(char c) {
-    switch (c) {
-        case '0': return 0;
-        case '1': return 1;
-        case '2': return 2;
-        case '3': return 3;
-        case '4': return 4;
-        case '5': return 5;
-        case '6': return 6;
-        case '7': return 7;
-        case '8': return 8;
-        case '9': return 9;
-        default: return 0;
-    }
-};
 
 int main(void) {
     cin.tie(0);
