294f13ff33f6d1f1e86c598ae239c2aa4f387c73
y-oksaku <y.oksaku@stu.kanazawa-u.ac.jp>
2019-10-21 22:38:55 +0900

    streak

---
+++ code/other/全国統一プログラミング王決定戦予選/a.rs
@@ -0,0 +1,63 @@
+#![allow(unused_imports)]
+#![allow(unused_macros)]
+#![allow(non_snake_case)]
+#![allow(dead_code)]
+#![allow(unused_variables)]
+#![allow(unused_mut)]
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+use std::cmp::{max, min, Ordering};
+
+macro_rules! get {
+    ($t:ty) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    ($t:ty; $n:expr) => {
+        (0..$n).map(|_|
+            get!($t)
+        ).collect::<Vec<_>>()
+    };
+    ($($t:ty),*; $n:expr) => {
+        (0..$n).map(|_|
+            get!($($t),*)
+        ).collect::<Vec<_>>()
+    };
+    ($t:ty ;;) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.split_whitespace()
+                .map(|t| t.parse::<$t>().unwrap())
+                .collect::<Vec<_>>()
+        }
+    };
+    ($t:ty ;; $n:expr) => {
+        (0..$n).map(|_| get!($t ;;)).collect::<Vec<_>>()
+    };
+}
+
+// N M => let (N, M) = get!(usize, usize)
+// A1 B1
+// ...
+// AQ BQ => let A = get!(usize, usize; Q)
+
+const MOD: u64 = 1000000007;
+
+fn main() {
+    let (N, X, Y) = get!(i32, i32, i32);
+
+    println!("{} {}", min(X, Y), max(0, X + Y - N));
+}
