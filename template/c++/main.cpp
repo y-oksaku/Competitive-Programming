51a0ff140feee7941cc5a3efdf71df17821d6798
y-oksaku <y.oksaku@stu.kanazawa-u.ac.jp>
2019-11-04 22:10:58 +0900

    テンプレート更新 C++ debug追加

--- template/c++/main.cpp
+++ template/c++/main.cpp
@@ -14,6 +14,94 @@ using namespace std;
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
 
+template <typename A, typename B>
+string to_string(pair<A, B> p);
+
+template <typename A, typename B, typename C>
+string to_string(tuple<A, B, C> p);
+
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p);
+
+string to_string(const string& s) {
+    return '"' + s + '"';
+}
+
+string to_string(const char* s) {
+    return to_string((string) s);
+}
+
+string to_string(bool b) {
+    return (b ? "true" : "false");
+}
+
+string to_string(vector<bool> v) {
+    bool first = true;
+    string res = "{";
+    for (int i = 0; i < static_cast<int>(v.size()); i++) {
+        if (!first) {
+            res += ", ";
+        }
+        first = false;
+        res += to_string(v[i]);
+    }
+    res += "}";
+    return res;
+}
+
+template <size_t N>
+string to_string(bitset<N> v) {
+    string res = "";
+    for (size_t i = 0; i < N; i++) {
+        res += static_cast<char>('0' + v[i]);
+    }
+    return res;
+}
+
+template <typename A>
+string to_string(A v) {
+    bool first = true;
+    string res = "{";
+    for (const auto &x : v) {
+        if (!first) {
+            res += ", ";
+        }
+        first = false;
+        res += to_string(x);
+    }
+    res += "}";
+    return res;
+}
+
+template <typename A, typename B>
+string to_string(pair<A, B> p) {
+    return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+
+template <typename A, typename B, typename C>
+string to_string(tuple<A, B, C> p) {
+    return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ")";
+}
+
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p) {
+    return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ", " + to_string(get<3>(p)) + ")";
+}
+
+void debug_out() { cerr << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+    cerr << " " << to_string(H);
+    debug_out(T...);
+}
+
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
 using Int = unsigned int;
 using llong = long long;
 using Llong = unsigned long long;
@@ -38,9 +126,8 @@ using desc = greater<T>();
 const llong MOD = 1e9 + 7;
 const llong INF = 1e17;
 
-#define FOR(i, n) for (llong i = 0; i < n; i++)
-#define FORS(i, a, b) for (llong i = a; i < b; i++)
-#define FORR(i, n) for (llong i = n; i > 0; i++)
+#define FOR(i, n) for (llong i = 0LL; i < llong(n); i++)
+#define FORS(i, a, b) for (llong i = llong(a); i < llong(b); i++)
 #define sup(vec, a) upper_bound(vec.begin(), vec.end(), a) - vec.begin()
 #define inf(vec, a) lower_bound(vec.begin(), vec.end(), a) - vec.begin()
 
@@ -50,6 +137,8 @@ int main(void) {
 
     llong N;
     cin >> N;
+
+    debug(N);
 
     return 0;
 }
