cd881d638c1da57adc331af889e4300a3c902ec9
y-oksaku <y.oksaku@stu.kanazawa-u.ac.jp>
2020-02-09 14:04:01 +0900

    recommend

---
+++ AtCoder/arc/094c.py
@@ -0,0 +1,18 @@
+N = int(input())
+A = []
+B = []
+for _ in range(N):
+    a, b = map(int, input().split())
+    A.append(a)
+    B.append(b)
+
+if(A == B):
+    print(0)
+    exit()
+
+ans = sum(A)
+dis = 10**10
+for a, b in zip(A, B):
+    if a > b:
+        dis = min(dis, b)
+print(ans - dis)
