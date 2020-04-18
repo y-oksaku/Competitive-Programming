7401e67ab05015ff75e4a5d381941b6ee1e69094
y-oksaku <y.oksaku@stu.kanazawa-u.ac.jp>
2020-04-18 09:30:33 +0900

    twitter

---
+++ AtCoder/abc/037d_2.cpp
@@ -0,0 +1,169 @@
+#include <map>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <bitset>
+#include <cassert>
+#include <queue>
+#include <random>
+#include <stack>
+#include <iomanip>
+#include <functional>
+
+using namespace std;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+template <class A, class B>
+string to_string(pair<A, B> p);
+
+template <class A, class B, class C>
+string to_string(tuple<A, B, C> p);
+
+template <class A, class B, class C, class D>
+string to_string(tuple<A, B, C, D> p);
+
+string to_string(const string& s) {
+    return '"' + s + '"';
+}
+
+string to_string(const char* s) {
+    return to_string((string) s);
+}
+
+string to_string(bool b) {
+    return (b ? "true" : "false");
+}
+
+string to_string(vector<bool> v) {
+    bool first = true;
+    string res = "{";
+    for (int i = 0; i < static_cast<int>(v.size()); i++) {
+        if (!first) {
+            res += ", ";
+        }
+        first = false;
+        res += to_string(v[i]);
+    }
+    res += "}";
+    return res;
+}
+
+template <size_t N>
+string to_string(bitset<N> v) {
+    string res = "";
+    for (size_t i = 0; i < N; i++) {
+        res += static_cast<char>('0' + v[i]);
+    }
+    return res;
+}
+
+template <class A>
+string to_string(A v) {
+    bool first = true;
+    string res = "{";
+    for (const auto &x : v) {
+        if (!first) {
+            res += ", ";
+        }
+        first = false;
+        res += to_string(x);
+    }
+    res += "}";
+    return res;
+}
+
+template <class A, class B>
+string to_string(pair<A, B> p) {
+    return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+
+template <class A, class B, class C>
+string to_string(tuple<A, B, C> p) {
+    return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ")";
+}
+
+template <class A, class B, class C, class D>
+string to_string(tuple<A, B, C, D> p) {
+    return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ", " + to_string(get<3>(p)) + ")";
+}
+
+void debug_out() { cerr << endl; }
+
+template <class Head, class... Tail>
+void debug_out(Head H, Tail... T) {
+    cerr << " " << to_string(H);
+    debug_out(T...);
+}
+
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+using Int = unsigned int;
+using llong = long long;
+using Llong = unsigned long long;
+using ldouble = long double;
+using intV = vector<int>;
+using llongV = vector<long long>;
+using llongVV = vector<vector<long long>>;
+
+template<class T = int>
+using asc_pque = priority_queue<T, vector<T>, greater<T>>;
+template<class T = int>
+using desc_pque = priority_queue<T, vector<T>, less<T>>;
+
+const llong MOD = 1000000007;
+const int IINF = 1000000000;
+const llong LINF = 100000000000000000LL;
+
+#define FOR(i, n) for (llong i = 0LL; i < llong(n); i++)
+#define FORS(i, a, b) for (llong i = llong(a); i < llong(b); i++)
+#define bisect_right(vec, a) upper_bound(vec.begin(), vec.end(), a) - vec.begin()
+#define bisect_left(vec, a) lower_bound(vec.begin(), vec.end(), a) - vec.begin()
+#define GET(i, T) get<i>(T)
+
+int main(void) {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    llong H, W;
+    cin >> H >> W;
+
+    llongVV A(H + 2, llongV(W + 2, -LINF));
+    vector<tuple<llong, llong, llong>> order;
+    FORS(h, 1, H + 1) {
+        FORS(w, 1, W + 1) {
+            cin >> A[h][w];
+            order.emplace_back(A[h][w], h, w);
+        }
+    }
+
+    llongVV B(H + 2, llongV(W + 2, 1LL));
+
+    sort(order.begin(), order.end());
+    for(auto e : order) {
+        llong a, h, w;
+        tie(a, h, w) = e;
+        llong n = B[h][w];
+
+        if(A[h + 1][w] > a) B[h + 1][w] = (B[h + 1][w] + n) % MOD;
+        if(A[h - 1][w] > a) B[h - 1][w] = (B[h - 1][w] + n) % MOD;
+        if(A[h][w + 1] > a) B[h][w + 1] = (B[h][w + 1] + n) % MOD;
+        if(A[h][w - 1] > a) B[h][w - 1] = (B[h][w - 1] + n) % MOD;
+    }
+
+    llong ans = 0;
+    FORS(h, 1, H + 1) {
+        FORS(w, 1, W + 1) {
+            ans = (ans + B[h][w]) % MOD;
+        }
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
