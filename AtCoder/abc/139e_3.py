acb65f70f068e438fed08b6b7218ab01b040ff4a
y-oksaku <y.oksaku@stu.kanazawa-u.ac.jp>
2020-05-22 09:05:03 +0900

    あさかつ

---
+++ AtCoder/abc/139e_3.py
@@ -0,0 +1,27 @@
+from collections import deque, defaultdict
+
+N = int(input())
+A = [deque(map(lambda a: int(a) - 1, input().split())) for _ in range(N)]
+
+D = [0] * N
+Nx = [-1] * N
+que = deque(range(N))
+
+while que:
+    i = que.popleft()
+    j = A[i].popleft()
+    Nx[i] = j
+
+    if Nx[j] == i:
+        day = max(D[i], D[j])
+        D[i] = D[j] = day + 1
+
+        if A[i]:
+            que.append(i)
+        if A[j]:
+            que.append(j)
+
+if all(len(a) == 0 for a in A):
+    print(max(D))
+else:
+    print(-1)
